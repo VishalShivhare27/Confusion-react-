617f39e0084bb97bfd5746c8f19652c15396293a
Vishal Shivhare <vishalshivhare27021998@gmail.com>
2020-05-07 12:13:49 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import {Card, CardImg,CardText,CardBody,CardTitle,Modal,ModalHeader,ModalBody,Breadcrumb, BreadcrumbItem,Button,Row,Col,Label} from 'reactstrap';
   import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Link} from 'react-router-dom';
+import {Loading} from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -159,8 +160,27 @@ class CommentForm extends Component {
 
 
     const DishDetail = (props) => {
-    	const dish = props.dish
-        if (dish == null) {
+      const dish = props.dish
+      if(props.isLoading){
+        return(
+          <div className="container">
+            <div className="row">
+              <Loading />
+            </div>
+          </div>
+        );
+      }
+      else if(props.errMess){
+        return(
+          <div className="container">
+          <div className="row">
+        <h4> {props.errMess}</h4>
+          </div>
+        </div>
+          
+        );
+      }
+      else if (dish == null) {
             return (<div></div>)
         }
         const dishItem = <RenderDish dish={props.dish}/>
